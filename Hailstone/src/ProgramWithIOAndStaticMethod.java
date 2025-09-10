3cf584a0d92886e7a35b1bc73cef686ba3657d8b
zzzzzz123zzz <zizhengzhuang9@gmail.com>
2025-09-09 21:47:24 -0400

    Add files via upload

---
+++ Hailstone/src/ProgramWithIOAndStaticMethod.java
@@ -0,0 +1,69 @@
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+
+/**
+ * Put a short phrase describing the program here.
+ *
+ * @author Put your name here
+ *
+ */
+public final class ProgramWithIOAndStaticMethod {
+
+    /**
+     * No argument constructor--private to prevent instantiation.
+     */
+    private ProgramWithIOAndStaticMethod() {
+    }
+
+    /**
+     * Generates and outputs the Hailstone series starting with the given
+     * integer.
+     *
+     * @param n
+     *            the starting integer
+     * @param out
+     *            the output stream
+     */
+    private static void generateSeries(int n, SimpleWriter out) {
+        out.print(n);
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+
+            } else {
+                n = 3 * n + 1;
+            }
+            out.print(", " + n);
+        }
+        out.println();
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        SimpleReader in = new SimpleReader1L();
+        SimpleWriter out = new SimpleWriter1L();
+        /*
+         * Put your main program code here; it may call myMethod as shown
+         */
+        out.println("Enter a positive integer: ");
+        int n = in.nextInteger();
+        while (n <= 0) {
+            out.print("Please enter a positive integer: ");
+            n = in.nextInteger();
+        }
+        generateSeries(n, out);
+        /*
+         * Close input and output streams
+         */
+        in.close();
+        out.close();
+    }
+
+}
